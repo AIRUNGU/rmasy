c37e90aa245154ab921c2a20f6d64193540f0790
ayub <ayubndururi@gmail.com>
2020-08-03 21:41:47 +0300

    channels commits3

--- reporter/routing.py
+++ reporter/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from reporter import consumers
 
 websocket_urlpatterns = [
-	path('wss/report/<str:room_name>/', consumers.ChatConsumer),
+	path('ws/report/<str:room_name>/', consumers.ChatConsumer),
 ]
