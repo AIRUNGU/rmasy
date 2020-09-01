5c4864a4fb62891925b3edf7060322dbda490fe9
ayub <ayubndururi@gmail.com>
2020-09-01 18:44:03 +0300

    wss

--- reporter/routing.py
+++ reporter/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from reporter import consumers as r_con
 
 websocket_urlpatterns = [
-    path('ws/report/<str:code>/', r_con.ReportPlatform),
+    path('wss/report/<str:code>/', r_con.ReportPlatform),
 ]
